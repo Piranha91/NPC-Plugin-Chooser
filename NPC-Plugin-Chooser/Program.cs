14264d6b7505ac7754ba25c30a82c83b3cea53b1
Piranha91 <piranha9191@gmail.com>
2021-05-01 21:46:06 -0700

    Revert "Merge remote-tracking branch 'origin/Dev' into Dev"
    This reverts commit 00857a9b8ec19b22f992d5f1f80e87921e03db44.

--- NPC-Plugin-Chooser/Program.cs
+++ NPC-Plugin-Chooser/Program.cs
@@ -144,7 +144,6 @@ namespace NPCPluginChooser
                             {
                                 string NPCdispStr = npc.Name + " | " + npc.EditorID + " | " + npc.FormKey.ToString();
                                 Console.WriteLine("Forwarding appearance of {0}", NPCdispStr);
-                                Console.WriteLine("Debug: {0} {1}", currentModContext.ModKey.ToString(), currentDataDir);
                                 if (faceGenExists(npc.FormKey, currentModContext.ModKey, currentDataDir, PPS.ExtraDataDirectories, settings.HandleBSAFiles_Patching, state, out var BSAfiles) == false)
                                 {
                                     if (settings.AbortIfMissingFaceGen)
